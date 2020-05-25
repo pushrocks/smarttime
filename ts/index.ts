5ebd9ce6def4d8b6d5e4223a89128081ccfe400d
Phil Kunz <gitlab@philkunz.com>
2020-05-25 22:21:55 +0000

    fix(core): update

--- ts/index.ts
+++ ts/index.ts
@@ -1,10 +1,6 @@
-import * as plugins from './smarttime.plugins';
-
 export * from './smarttime.classes.cronmanager';
 export * from './smarttime.classes.extendeddate';
 export * from './smarttime.classes.hrtmeasurement';
 export * from './smarttime.classes.timer';
 export * from './smarttime.classes.timestamp';
 export * from './smarttime.units';
-
-export { luxon } from './smarttime.plugins';
