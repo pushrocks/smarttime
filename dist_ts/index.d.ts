3f0b230d239bba5d73817a713387ff123fe07918
Phil Kunz <gitlab@philkunz.com>
2020-05-25 21:49:08 +0000

    fix(core): update

--- dist_ts/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './smarttime.classes.cronmanager';
-export * from './smarttime.classes.extendeddate';
-export * from './smarttime.classes.hrtmeasurement';
-export * from './smarttime.classes.timer';
-export * from './smarttime.classes.timestamp';
-export * from './smarttime.units';
-export { luxon } from './smarttime.plugins';
--- dist_ts/smarttime.plugins.d.ts
+++
@@ -1,6 +0,0 @@
-import * as smartdelay from '@pushrocks/smartdelay';
-import * as smartpromise from '@pushrocks/smartpromise';
-export { smartdelay, smartpromise };
-import cronParser from 'cron-parser';
-import * as luxon from 'luxon';
-export { cronParser, luxon };
